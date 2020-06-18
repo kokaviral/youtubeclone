db7d91e4ad1d0c6f73b87ef5bc2cb61a633783cc
Aviral <kokaviral@gmail.com>
2020-06-18 15:40:12 +0530

    changes

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import './searchbar.css'
 
-class Searchbar extends React.Component
-{
-    state = {searchterm:''}
+class Searchbar extends React.Component {
+    state = { searchterm: '' }
 
-    onInputChange =   (event)  =>  {
-        this.setState({searchterm:event.target.value});
+    onInputChange = (event) => {
+        this.setState({ searchterm: event.target.value });
     }
-    onFormSubmit    =   (event) =>  {
+    onFormSubmit = (event) => {
         event.preventDefault();
 
         this.props.termExtraction(this.state.searchterm)
     }
-    render()
-    {
-        return(
-            
+    render() {
+        return (
+
             <div className="ui segment searchbar" >
                 <form className="ui form" onSubmit={this.onFormSubmit} >
-                    <div className="field" style={{display:"flex",justifyContent:"between",borderRadius:"0"}}>
-                    <div style={{display:"flex",alignItems:"center"}}>
-                    <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/youtube-512.png" style={{width:"35px"}}/>
-                    <p id="para">YouTube</p>
-                    </div>
+                    <div className="field" style={{ display: "flex", justifyContent: "between", borderRadius: "0" }}>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                            <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/youtube-512.png" style={{ width: "35px" }} />
+                            <p id="para">YouTube</p>
+                        </div>
                         <input
                             type="text"
-                            placeholder="Search"
+                            placeholder="Search a video"
                             value={this.state.searchterm}
                             onChange={this.onInputChange}
-                            style={{width:"50%",borderRadius:"0",fontFamily:"font-family: 'Roboto', sans-serif;",marginLeft:"17%"}}
+                            style={{ width: "50%", borderRadius: "0", fontFamily: "font-family: 'Roboto', sans-serif;", marginLeft: "17%" }}
                         />
-                        <button className="ui icon button" style={{borderRadius:"0"}} onClick={this.onFormSubmit}>
-                            <i className="search icon" style={{borderRadius:"0",paddingLeft:"10px",paddingRight:"25px"}}/>
+                        <button className="ui icon button" style={{ borderRadius: "0" }} onClick={this.onFormSubmit}>
+                            <i className="search icon" style={{ borderRadius: "0", paddingLeft: "10px", paddingRight: "25px" }} />
                         </button>
                     </div>
                 </form>
